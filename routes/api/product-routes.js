89904de793e9ca9affcc897ba477e1b39e3ae70d
Lo Selby <laurenxselby@gmail.com>
2022-02-12 20:34:54 -0800

    added category id to product create route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -52,6 +52,7 @@ router.post("/", (req, res) => {
     product_name: req.body.product_name,
     price: req.body.price,
     stock: req.body.stock,
+    category_id: req.body.category_id,
     tagIds: req.body.tagIds,
   })
     .then((product) => {
